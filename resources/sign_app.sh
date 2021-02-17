9225e8bdc4e30e6ef15ff5a918c3a875e44cbb21
GohanGokuGoten <gohangoku10@protonmail.com>
2021-02-17 15:36:56 -0300

    Fix data from onesignal

--- resources/sign_app.sh
+++ resources/sign_app.sh
@@ -1,4 +1,4 @@
 ionic cordova plugin rm cordova-plugin-console
 ionic cordova build --release --prod android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore resources/google_play/westnet.keystore -storepass W3stn3T**20I8// platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk westnet_app
-zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk Westnet_1.2.1.apk
+zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk Westnet_1.2.4.apk
--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,24 +1,24 @@
 export let CONFIG = {
     //compu Juanpi
-    apiUrl: 'http://192.168.2.80:8180/web/index.php?r=mobileapp/v1',
+    //apiUrl: 'http://192.168.2.80:8180/web/index.php?r=mobileapp/v1',
     // apiUrl: 'http://67721f36df8a.ngrok.io/web/index.php?r=mobileapp/v1',
     //compu Janet
     // apiUrl: 'http://192.168.0.108:8180/web/index.php?r=mobileapp/v1',
     //server testing
     //apiUrl: 'http://168.227.99.20/index.php?r=mobileapp/v1',
     //server produccion
-    //apiUrl: 'https://gestion.westnet.com.ar/index.php?r=mobileapp/v1',
+    apiUrl: 'https://gestion.westnet.com.ar/index.php?r=mobileapp/v1',
     pdfUrl: '/user-app/bill-pdf&pdf_key=',
     privateToken: 'west123456net',
     currency: 'ARG',
     currencySymbol: '$',
     //Produccion
-    //oneSignalAppID: '3222eb5d-9c88-41c5-b3c4-053bf2671228',
-    //senderIdFirebase: '514016560616',
-    //webClientGoogle: '514016560616-6pt54kglpssj1lio7bu6du3qt1viagsa.apps.googleusercontent.com'
+    oneSignalAppID: '3222eb5d-9c88-41c5-b3c4-053bf2671228',
+    senderIdFirebase: '514016560616',
+    webClientGoogle: '514016560616-6pt54kglpssj1lio7bu6du3qt1viagsa.apps.googleusercontent.com'
 
     //Testing
-     oneSignalAppID: '88ff4e04-2fce-4479-a676-9a978fc6046c',
-     webClientGoogle: '514016560616-6pt54kglpssj1lio7bu6du3qt1viagsa.apps.googleusercontent.com',
-     senderIdFirebase: '512594129842',
+    // oneSignalAppID: '88ff4e04-2fce-4479-a676-9a978fc6046c',
+    // webClientGoogle: '514016560616-6pt54kglpssj1lio7bu6du3qt1viagsa.apps.googleusercontent.com',
+    // senderIdFirebase: '512594129842',
 };
